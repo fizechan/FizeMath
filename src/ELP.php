45f7f5049dc7005538cfdc62f6d3a85d1e72fc29
FizeChan <chenfengzhan@qq.com>
2021-03-09 09:52:48 +0800

    新增算法：等额本息。

---
+++ src/ELP.php
@@ -0,0 +1,172 @@
+<?php
+
+
+namespace fize\math;
+
+
+/**
+ * 等额本息
+ */
+class ELP
+{
+    /**
+     * @var float 贷款金额
+     */
+    protected $capital;
+
+    /**
+     * @var float 年利率
+     */
+    protected $annualInterestRate;
+
+    /**
+     * @var int 还款期数
+     */
+    protected $issues;
+
+    /**
+     * @var int 每期月数
+     */
+    protected $issueMonths;
+
+    /**
+     * 初始化时设置参数
+     * @param float $capital              贷款金额
+     * @param float $annual_interest_rate 年利率
+     * @param int   $issues               还款期数
+     * @param int   $issue_months         每期月数
+     */
+    public function __construct($capital, $annual_interest_rate, $issues, $issue_months = 1)
+    {
+        $this->capital = $capital;
+        $this->annualInterestRate = $annual_interest_rate;
+        $this->issues = $issues;
+        $this->issueMonths = $issue_months;
+    }
+
+    /**
+     * 月利率
+     * @return float
+     */
+    public function monthlyInterestRate()
+    {
+        return $this->annualInterestRate / 12;
+    }
+
+    /**
+     * 期利率
+     * @return float
+     */
+    public function issueInterestRate()
+    {
+        $gap = 12 / $this->issueMonths;
+        return $this->annualInterestRate / $gap;
+    }
+
+    /**
+     * 每期还款金额
+     * @return float
+     */
+    public function issueRent()
+    {
+        $issue_ir = $this->issueInterestRate();
+        $numerator = $this->capital * $issue_ir * pow(1 + $issue_ir, $this->issues);
+        $denominator = pow(1 + $issue_ir, $this->issues) - 1;
+        return $numerator / $denominator;
+    }
+
+    /**
+     * 还款总额
+     * @return float
+     */
+    public function totalRent()
+    {
+        return $this->issueRent() * $this->issues;
+    }
+
+    /**
+     * 总利息
+     * @return float
+     */
+    public function totalInterest()
+    {
+        return $this->totalRent() - $this->capital;
+    }
+
+    /**
+     * 租金计划
+     * @param int   $scale           保留小数位
+     * @param int   $fix_issue       修正的期数
+     * @param false $with_issue_zero 是否附带第0期(即放款期)
+     * @return array 下标 => [租金, 本金, 利息]
+     */
+    public function plans($scale = 2, $fix_issue = -1, $with_issue_zero = false)
+    {
+        $plans = [];
+        $plans[] = [-$this->capital, -$this->capital, 0];  // 第0期为放款
+        $issue_rent = round($this->issueRent(), $scale);
+
+        $capital_balance = $this->capital;
+        for ($issue = 1; $issue <= $this->issues; $issue++) {
+            $interest = round($capital_balance * $this->issueInterestRate(), $scale);  // 利息
+            $capital = round($issue_rent - $interest, 2);  // 本金
+            $plans[$issue] = [$issue_rent, $capital, $interest];
+            $capital_balance = round($capital_balance - $capital, 2);
+        }
+
+        // 修正一期使全部本金相加等于总本金
+        if ($fix_issue < 0) {
+            $fix_issue = count($plans) + $fix_issue;
+        }
+        $capital_sum = 0.00;
+        for ($issue = 1; $issue <= $this->issues; $issue++) {
+            if ($issue != $fix_issue) {
+                $capital_sum = round($capital_sum + $plans[$issue][1]);
+            }
+        }
+        $capital_fix = round($this->capital - $capital_sum, $scale);
+        $interest_fix = round($issue_rent - $capital_fix, $scale);
+        $plans[$fix_issue] = [$issue_rent, $capital_fix, $interest_fix];
+
+        if (!$with_issue_zero) {
+            array_shift($plans);
+        }
+
+        return $plans;
+    }
+
+    /**
+     * 含日期信息的租金计划
+     * @param string $loan_date 起租日期
+     * @param int    $scale 保留小数位
+     * @param int    $fix_issue 修正的期数
+     * @param false  $with_issue_zero 是否附带第0期(即放款期)
+     * @return array 日期 => [租金, 本金, 利息]
+     */
+    public function datePlans($loan_date, $scale = 2, $fix_issue = -1, $with_issue_zero = false)
+    {
+        $plans = $this->plans($scale, $fix_issue, true);
+        $datePlans = [];
+        for ($issue = 0; $issue <= $this->issues; $issue++) {
+            $datePlans[$this->getIssueDate($loan_date, $issue)] = $plans[$issue];
+        }
+        if (!$with_issue_zero) {
+            unset($datePlans[$loan_date]);
+        }
+        return $datePlans;
+    }
+
+    /**
+     * 获取指定期的日期
+     * @param string $loan_date 起租日期
+     * @param int    $issue     期数
+     * @return string
+     */
+    private function getIssueDate($loan_date, $issue)
+    {
+        list($sYear, $sMonth) = explode('-', $loan_date);
+        $monthend = date("Y-m-d", strtotime("+" . ($this->issueMonths * $issue + 1) . " month -1 day", strtotime($sYear . '-' . $sMonth . '-01')));
+        $nextDate = date("Y-m-d", strtotime("+" . ($this->issueMonths * $issue) . " month", strtotime($loan_date)));
+        return $nextDate > $monthend ? $monthend : $nextDate;
+    }
+}
---
+++ tests/TestELP.php
@@ -0,0 +1,72 @@
+<?php
+
+
+use fize\math\ELP;
+use PHPUnit\Framework\TestCase;
+
+class TestELP extends TestCase
+{
+
+    public function test__construct()
+    {
+        $elp = new ELP(1180000.00, 0.05, 360);
+        var_dump($elp);
+        self::assertIsObject($elp);
+    }
+
+    public function testMonthlyInterestRate()
+    {
+        $elp = new ELP(1180000.00, 0.05, 360);
+        $mir = $elp->monthlyInterestRate();
+        var_dump($mir);
+        self::assertIsFloat($mir);
+    }
+
+    public function testIssueInterestRate()
+    {
+        $elp = new ELP(1180000.00, 0.05, 360);
+        $iir = $elp->issueInterestRate();
+        var_dump($iir);
+        self::assertIsFloat($iir);
+    }
+
+    public function testIssueRent()
+    {
+        $elp = new ELP(1180000.00, 0.05, 360);
+        $rent = $elp->issueRent();
+        var_dump($rent);
+        self::assertIsFloat($rent);
+    }
+
+    public function testTotalRent()
+    {
+        $elp = new ELP(1180000.00, 0.05, 360);
+        $rent = $elp->totalRent();
+        var_dump($rent);
+        self::assertIsFloat($rent);
+    }
+
+    public function testTotalInterest()
+    {
+        $elp = new ELP(1180000.00, 0.05, 360);
+        $interest = $elp->totalInterest();
+        var_dump($interest);
+        self::assertIsFloat($interest);
+    }
+
+    public function testPlans()
+    {
+        $elp = new ELP(1180000.00, 0.05, 360);
+        $plans = $elp->plans();
+        var_export($plans);
+        self::assertIsArray($plans);
+    }
+
+    public function testDatePlans()
+    {
+        $elp = new ELP(1180000.00, 0.05, 360);
+        $date_plans = $elp->datePlans('2021-06-30');
+        var_export($date_plans);
+        self::assertIsArray($date_plans);
+    }
+}
